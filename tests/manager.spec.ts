eb3d696d68989cb69ba210ccecc8195f69f373cc
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-08-17 14:24:26 +0200

    add a11y e2e

--- tests/manager.spec.ts
+++ tests/manager.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("manager", async () => {
   test.beforeEach(async ({ page }) => {
@@ -10,5 +11,13 @@ test.describe("manager", async () => {
   test("page loads and has correct title", async ({ page }) => {
     // verify that title is correct
     expect(await page.title()).toContain("Space Haven Manager");
+  });
+
+  // https://playwright.dev/docs/accessibility-testing
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
