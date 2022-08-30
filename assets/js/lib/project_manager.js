a2bfb26637890a633cea4c3cbf20653936627bef
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-08-30 14:29:37 +0200

    add another direction and z index

--- assets/js/lib/project_manager.js
+++ assets/js/lib/project_manager.js
@@ -1,7 +1,7 @@
 import debounce from "debounce";
 
 let stars = [];
-const settings = { amount: 20, speed: 3, size: 3 };
+const settings = { amount: 150, speed: 2, baseSize: 1, direction: "right" };
 
 // generate random number between 0.8 and 1.0
 // const random = () => Math.random() * 0.2 + 0.8;
@@ -13,7 +13,7 @@ const generateStars = () => {
     s.push({
       x: Math.random() * window.innerWidth,
       y: Math.random() * window.innerHeight,
-      size: settings.size * Math.random() + 1,
+      size: settings.baseSize * Math.random() * 2 + 1,
     });
   }
   stars = s;
@@ -36,18 +36,31 @@ const draw = () => {
   ctx.beginPath();
   for (let i = 0; i < stars.length; i++) {
     const star = stars[i];
-    star.x += settings.speed;
-    star.y -= settings.speed;
-    ctx.moveTo(stars[i].x, stars[i].y);
+    if (settings.direction === "left") {
+      star.x -= settings.speed * (star.size / 2);
+      star.y += settings.speed * (star.size / 2);
+      ctx.moveTo(stars[i].x, stars[i].y);
 
-    if (star.x > w) {
-      star.x = 0;
-    }
-    if (star.y < 0) {
-      star.y = h;
+      if (star.x < 0) {
+        star.x = w;
+      }
+      if (star.y > h) {
+        star.y = 0;
+      }
+    } else if (settings.direction === "right") {
+      star.x += settings.speed * (star.size / 2);
+      star.y -= settings.speed * (star.size / 2);
+      ctx.moveTo(stars[i].x, stars[i].y);
+
+      if (star.x > w) {
+        star.x = 0;
+      }
+      if (star.y < 0) {
+        star.y = h;
+      }
     }
 
-    ctx.fillStyle = `rgba(255, 255, 255, ${1 /* Math.random() * 0.2 + 0.8 */})`;
+    ctx.fillStyle = `rgba(255, 255, 255, ${Math.random() * 0.2 + 0.7})`;
     ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
   }
   ctx.fill();
