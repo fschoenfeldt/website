200de2955206b07d987f8c0d626bd32e2046b7c1
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-12-27 16:50:03 +0100

    allow edit mode on vacation page (wip)

--- assets/js/lib/vacation.js
+++ assets/js/lib/vacation.js
@@ -14,4 +14,26 @@ const format = (diffDays) => `${getDaysBetween(diffDays)} Tage`;
 export default () => ({
   getDaysBetween: getDaysBetween,
   format: format,
+  editMode: false,
+  generatedLink: null,
+  handleSubmit: (event) => {
+    event.preventDefault();
+    const date = event.target.date.value;
+    const currentUrl = location.href.replace(location.search, "");
+    const newParams = new URLSearchParams({ date }).toString();
+    const generatedLink = `${currentUrl}?${newParams}`;
+
+    // TODO: show generated link. Cant' access `this` here because we're in an event.
+  },
+  init: () => {
+    const params = new URL(document.location).searchParams;
+    const date = params.get("date");
+
+    // TODO: use custom date if present in params
+    if (date) {
+      console.log("i got date", date);
+    } else {
+      console.log("no date");
+    }
+  },
 });
