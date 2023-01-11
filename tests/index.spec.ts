92467ca503f959e08e5dea0875e37c1b44f128e1
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-01-11 16:00:17 +0100

    close cookie consent before screenshots on index page

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,6 +6,14 @@ test.describe("index/home page", () => {
     await page.goto("/");
     // browsersync redirects to correct page so we have to wait
     await page.waitForLoadState("networkidle");
+
+    const cookieConsent = await page.$("#cm");
+    if (cookieConsent) {
+      // wait for animation to be finished
+      await page.waitForSelector(".c--anim", { state: "attached" });
+      const rejectButton = await cookieConsent.$("#c-s-bn");
+      rejectButton && (await rejectButton.click());
+    }
   });
 
   test("matches snapshot", async ({ page }) => {
