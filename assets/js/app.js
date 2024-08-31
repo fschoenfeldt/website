716557441f767b706118b2ebf60d67ddf86daaaf
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2024-08-31 12:33:51 +0200

    Feature/cv and cookie update (#22)
    * update cookie consent to reflect their v3 changes
    
    * update cv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
 import Alpine from "alpinejs";
-import "vanilla-cookieconsent";
+import * as CookieConsent from "vanilla-cookieconsent";
 import ccOptions from "./lib/cookie_consent";
 import projectManager from "./lib/project_manager";
 
 // https://github.com/orestbida/cookieconsent
-const cc = initCookieConsent();
-cc.run(ccOptions);
+CookieConsent.run(ccOptions);
 
 // initialize alpine
 window.Alpine = Alpine;
